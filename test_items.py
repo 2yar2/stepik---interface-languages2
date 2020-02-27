937e3cbb658b3d2fa034f32cf56969b4cc10e67a
2yar2 <404tm2@mail.ru>
2020-02-27 22:57:12 +0300

    languages
    запуск автотестов для разных языков интерфейса

---
+++ test_items.py
@@ -0,0 +1,7 @@
+﻿import time
+
+def test_assert_basket(browser):
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    time.sleep(30)
+    assert browser.find_elements_by_css_selector('.btn.btn-lg.btn-primary.btn-add-to-basket'), \
+        'Nashol`'
